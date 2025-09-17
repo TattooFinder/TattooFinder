9d02ea84a3e8e83e4ef7ea6ab1bc7554f781d625
Unifor <unifor@gmail.com>
2025-09-17 20:30:46 -0300

    new web fixed this dummy thing

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,12 +1,10 @@
 from app import db
 
-class User(db.Model):
-    __tablename__ = "usuário"
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
 
-    id = db.Column(db.Integer, primary_key = true)
-    nome = db.Column(db.String(100) nullable = false)
-    email = db.Column(db.String(100) unique = true
-    nullable = false)
-    senha = dbColumn(db.String(100) nullable = false)
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(200), nullable=False)
 
-    
